10a8ab175c8ce891d2cf68f177dc5657718f3961
tao.zhou <tao.zhou@trip.com>
2022-09-24 11:16:32 +0800

    TDD: BooleanOptionParserTest RED

---
+++ src/main/java/tdd/args/TooManyArgumentsException.java
@@ -0,0 +1,12 @@
+package tdd.args;
+
+/**
+ * @Author: tao.zhou
+ * @Date: 2022/9/24 11:09
+ */
+public class TooManyArgumentsException extends Exception {
+
+    public String getOption() {
+        return null;
+    }
+}
