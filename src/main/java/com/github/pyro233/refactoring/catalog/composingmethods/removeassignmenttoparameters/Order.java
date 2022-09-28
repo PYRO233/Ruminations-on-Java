1ab10922ce2bbcf50c401d44777c0adbe7cee407
tao.zhou <tao.zhou@trip.com>
2022-09-29 00:05:35 +0800

    Refactoring: removeassignmenttoparameters finish

--- src/main/java/com/github/pyro233/refactoring/catalog/composingmethods/removeassignmenttoparameters/Order.java
+++ src/main/java/com/github/pyro233/refactoring/catalog/composingmethods/removeassignmenttoparameters/Order.java
@@ -6,11 +6,12 @@ package com.github.pyro233.refactoring.catalog.composingmethods.removeassignment
  */
 public class Order {
 
-    public int discount(int inputVal, int quantity, int yearToDate) {
-        if (inputVal > 50) inputVal -= 2;
-        if (quantity > 100) inputVal -= 1;
-        if (yearToDate > 10000) inputVal -= 4;
-        return inputVal;
+    public int discount(final int inputVal, final int quantity, final int yearToDate) {
+        int result = inputVal;
+        if (inputVal > 50) result -= 2;
+        if (quantity > 100) result -= 1;
+        if (yearToDate > 10000) result -= 4;
+        return result;
     }
 
 }
