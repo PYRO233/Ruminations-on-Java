b41f0a131ae5d902f44db4233e836df8a99714d8
tao.zhou <tao.zhou@trip.com>
2022-10-11 00:36:09 +0800

    TDD: di.container To-do list

---
+++ src/test/java/com/github/pyro233/tdd/di/ContainerTest.java
@@ -0,0 +1,49 @@
+package com.github.pyro233.tdd.di;
+
+import org.junit.jupiter.api.Nested;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author: tao.zhou
+ * @Date: 2022/10/11 0:27
+ */
+class ContainerTest {
+
+    @Nested
+    public class ComponentConstruction {
+        //TODO: instance
+        //TODO: abstract class
+        //TODO: interface
+
+
+        @Nested
+        public class ConstructorInjection {
+            //TODO: No args constructor
+            //TODO: with dependencies
+            //TODO: A -> B -> C
+        }
+
+        @Nested
+        public class FieldInjection {
+
+        }
+
+        @Nested
+        public class MethodInjection {
+
+        }
+    }
+
+    @Nested
+    public class DependenciesSelection {
+
+    }
+
+
+    @Nested
+    public class LifecycleManagement {
+
+    }
+
+}
