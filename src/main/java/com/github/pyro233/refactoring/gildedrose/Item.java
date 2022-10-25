2c163c82c51bc119aa1a777c66ff4a1e6b0ee8ba
tao.zhou <tao.zhou@trip.com>
2022-10-25 17:55:58 +0800

    Refactoring: gildedrose (rename)

--- src/main/java/com/github/pyro233/refactoring/gildedrose/Item.java
+++ src/main/java/com/github/pyro233/refactoring/gildedrose/Item.java
@@ -7,18 +7,18 @@ package com.github.pyro233.refactoring.gildedrose;
 public class Item {
     public String name;
 
-    public int sell_in;
+    public int sellIn;
 
     public int quality;
 
     public Item(String name, int sell_in, int quality) {
         this.name = name;
-        this.sell_in = sell_in;
+        this.sellIn = sell_in;
         this.quality = quality;
     }
 
     @Override
     public String toString() {
-        return this.name + ", " + this.sell_in + ", " + this.quality;
+        return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
